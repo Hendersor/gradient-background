b9b83c179ebd052c3f6dc33cf41f118e7618fad2
Hendersor <montejof1998@gmail.com>
2021-03-04 12:38:09 -0800

    Javascript terminado

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hola perros")
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function color(){
+    body.style.background = "linear-gradient(to right, " + color1.value + " , " + color2.value + ")";
+}
+
+color1.addEventListener("input", function(){
+  color();
+})
+
+
+color2.addEventListener("input", function(){
+    color();
+    
+})
