c244099a1ca06dc17d45286f8d289d2b65b0fb08
Hendersor <montejof1998@gmail.com>
2021-03-04 12:45:23 -0800

    Se mejoro el codigo

--- script.js
+++ script.js
@@ -3,16 +3,10 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-function color(){
+function gradiente(){
     body.style.background = "linear-gradient(to right, " + color1.value + " , " + color2.value + ")";
 }
 
-color1.addEventListener("input", function(){
-  color();
-})
+color1.addEventListener("input", gradiente);
 
-
-color2.addEventListener("input", function(){
-    color();
-    
-})
+color2.addEventListener("input", gradiente);
